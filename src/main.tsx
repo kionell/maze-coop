2c541c9a8d55a8b4ef31b5a8708716e60832335d
Alexander <sashakostyunin1@gmail.com>
2023-05-12 21:59:37 +0300

    Remove default files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const element = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(element);
+
+root.render(<React.StrictMode><App /></React.StrictMode>);
