d2cf5a388df36542b3db48b97b233e7c93f337e5
Alexander <sashakostyunin1@gmail.com>
2023-05-30 01:36:40 +0300

    Serve client on the same domain

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,13 @@
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 (async function () {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3000;
 
-  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', '..', '..', 'public'));
 
   app.listen(port, async () => {
     const url = await app.getUrl();
