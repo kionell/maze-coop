2a2a84180d412e2193a4043777f2ab132624f9a1
Alexander <sashakostyunin1@gmail.com>
2023-05-30 01:39:28 +0300

    Add postgresql database and express sessions

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,8 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GameModule } from './game/game.module';
 import { RoomModule } from './room/room.module';
 import { ChatModule } from './chat/chat.module';
@@ -9,6 +12,18 @@ import { RedisModule } from './redis/redis.module';
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_DB_HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_DB_PORT) || 5432,
+      username: process.env.POSTGRES_DB_USERNAME || 'postgres',
+      password: process.env.POSTGRES_DB_PASSWORD || '123',
+      database: process.env.POSTGRES_DB_DATABASE || 'maze',
+      namingStrategy: new SnakeNamingStrategy(),
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
     }),
     GameModule,
     RoomModule,
